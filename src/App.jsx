412669103fd85cb5bec4267c420756f88889b3d4
FreelancerTasin507 <freelancertasin507@gmail.com>
2023-06-24 04:09:58 -0700

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header/Header'
-import Meals from './components/Meals/Meals'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-     <Header></Header>
-     <Meals></Meals>
+      {/* Header section */}
+      <Header></Header>
+      {/* Header section */}
+      {/* Meals section */}
+      <Meals></Meals>
+      {/* Meals section */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
